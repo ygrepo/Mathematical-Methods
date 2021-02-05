4b877cbcd474b4ce33753ae2440977481d03d9d2
Yves Greatti <yvgrotti@gmail.com>
2021-02-05 18:09:37 -0500

    Chapter 4 - problems completed

--- Module1_FourierSeriesI/ex_4_8.m
+++ Module1_FourierSeriesI/ex_4_8.m
@@ -2,14 +2,43 @@ clear all;
 close all;
 clc;
 
-%% Plot x sin(x) fpor [0, 2pi]
+%% Plot x sin(x) f for [0, 2pi]
+t = tiledlayout(2,2,'TileSpacing','Compact');
+
+nexttile
+x = linspace(-pi,pi);
+plot(x, x .* sin(x), "LineWidth",1,"MarkerSize",3);
+title('Even Extension','Interpreter','latex');
+xlabel("x");
+ylabel("f(x)")
+tick_values = -4:4;
+xlim([-4 4]);
+xticks(tick_values)
+grid on
+
+nexttile
+x1 = linspace(0,pi);
+x2 = linspace(-pi,0);
+plot(x1, x1 .* sin(x1),"r", x2, -x2 .* sin(x2), "r", "LineWidth",1,"MarkerSize",3);
+title('Odd Extension','Interpreter','latex');
+xlabel("x");
+ylabel("f(x)")
+xlim([-4 4]);
+xticks(tick_values)
+grid on
+
+saveas(gcf,'problem_4_8.pdf')
+
+%% Plot function defined in 4.26 
+
 figure, hold on, grid on
 xlim([-pi pi]);
 xlabel("x");
 ylabel("f(x)")
-x = linspace(-2*pi,2*pi);
-title('$f(x)= x \sin(x)$','Interpreter','latex');
-plot(x, x .* sin(x), "b-*", 'LineWidth',1,'MarkerSize',3);
-legend('x sin(x)', 'FontSize', 14, 'Location', 'NorthEast');
+syms y(x);
+y = piecewise((-pi<=x) & (x<0), -x .* (pi+ x), (0<=x) & (x<pi), x.* (x - pi));
+x = linspace(-pi,pi);
+fplot(y)
+saveas(gcf,'problem_4_26.pdf')
 
-saveas(gcf,'problem_4_8.pdf')
+
---
+++ module3_Integral_Transforms/problems.m
@@ -0,0 +1,7 @@
+clear all;
+close all;
+clc;
+
+%% Plot x sin(x) f for [0, 2pi]
+syms w
+y = exp(i .* w .* t)
