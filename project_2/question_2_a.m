8b48f0d9dd36f10a8cdc0905424d6d89d7d8925f
Yves Greatti <yvgrotti@gmail.com>
2021-03-27 12:11:27 -0400

    Project 2 in progress

---
+++ project_2/question_2_a.m
@@ -0,0 +1,18 @@
+%%
+clc
+clear all
+syms r 
+
+figure
+R = 10;
+%r = linspace(0,10, 10);
+Q = 2;
+PR = piecewise((0<=r) & (r<=10), Q * (1-r/R), r>R, 0);
+xlim([0 15])
+ylim([0 3])
+fplot(PR, [0 15], "-r", 'LineWidth',2,'MarkerSize',3);
+xlabel("r", "Interpreter","latex", 'FontSize', 14);
+ylabel("$P(r)$", "Interpreter","latex", 'FontSize', 14);
+title("$P(r)$ for $Q=2$, $R=10$", "Interpreter","latex", 'FontSize', 14);
+grid on
+saveas(gcf,"pr",'pdf')
