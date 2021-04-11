51128d925aaa9eb6af55438d1de15202efb031ed
Yves Greatti <yvgrotti@gmail.com>
2021-04-11 11:10:26 -0400

    Add module 12

---
+++ module12_complex_variables_I/homeworks/14_1.m
@@ -0,0 +1,33 @@
+%%
+clc
+clear all
+syms v x y
+v = (y* cos(y) + x * sin(y)) * exp(x)
+
+dudy = -diff(v,x);
+u = int(dudy,y);
+dudx = diff(u,x);
+dvdy = diff(v,y);
+dudy = diff(u,y);
+dvdx = diff(v,x);
+
+
+%%
+clc
+clear all
+syms x y
+y = log(x)^(1/x);
+diff(y,x);
+limit(y, x, Inf)
+y2 = log(log(x));
+diff(y2,x);
+y3 = (x^(log(x)))^(1/x);
+simplify(log(log(x)* log(x)));
+limit(y3, x, Inf)
+
+%%
+clc
+clear all
+syms z y
+y = sinh(1/z)
+limit(y,z,0)
